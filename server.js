38f03aad97e082de38b21da82e50518eccc5987e
Caleb Rogers <calebrogers@Calebs-MacBook-Pro.local>
2016-09-28 16:10:51 -0700

    core: server serving node modules

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
 
 app.use(bodyParser.json());
 
