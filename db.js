1e1f118850b347563ca5e622f9ba5052f0c490da
Caleb Rogers <calebrogers@Calebs-MacBook-Pro.local>
2016-09-28 15:27:55 -0700

    core: couple basic db methods

--- db.js
+++ db.js
@@ -1,5 +1,40 @@
 var api = {};
+var storage = {};
+var id = 0;
 
+api.getContacts = function(){
+    return new Promise((resolve, reject)=>{
+        var contacts = storage.keys();
+        if(!contacts.length){
+            reject();
+        }
+        else{
+            resolve(contacts);
+        }
+    });
+}
+
+api.createContact = function(newContact){
+    //TODO: somehow make sure it isnt a duplicate
+    storage[id] = newContact;
+    id++;
+    return new Promise((resolve, reject)=>{
+        resolve(newContact);
+    });
+}
+
+api.getContact = function(id){
+    return new Promise((resolve, reject)=>{
+        if(!storage[id]){
+            reject();
+        } else{
+            resolve(storage[id]);
+        }
+    });
+}
+
+//TODO: api.updateContact
+//TODO api.deleteContact
 
 
 
