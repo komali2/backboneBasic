6f15b6a652fd6b454fad4abd8d6073ad55c65b4e
Caleb Rogers <calebrogers@Calebs-MacBook-Pro.local>
2016-09-28 14:30:19 -0700

    core: basic server structure with todos listed

---
+++ server.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+
+//create a global database variable
+var db;
+
+
+//API ROUTES
+
+//basic error handle function
+function handleError(res, reason, message, code){
+    console.log('ERROR: ' + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+/*
+    "/contacts"
+    GET: finds all contacts
+    POST: creates a new contact
+*/
+
+app.get('/contacts', (req, res)=>{
+    //add db method get contacts
+});
+
+app.post('/contacts', (req, res)=>{
+    let newContact = req.body;
+    newContact.createDate = new Date();
+
+    if(!(req.body.firstName || req.body.lastName)){
+        handleError(res, 'Invalid User Input', 'Must provide a first and last name', 400);
+    }
+
+    //add db method create new contact
+});
+
+/*
+    "/contacts/:id"
+    GET: find contact by id
+    PUT: update contact by id
+    DELETE: deletes contact by id
+*/
+
+app.get('/contacts/:id', (req, res)=>{
+    // req.params.id
+    //add db method to get one contact by id
+});
+
+app.put('/contacts/:id', (req, res)=>{
+    let updateDoc = req.body;
+    delete updateDoc._id;
+
+    // req.params.id
+
+    //add db method to update the given contact
+});
+
+app.delete('/contacts/:id', (req, res)=>{
+    //add db method to delete given contact
+});
